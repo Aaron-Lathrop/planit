5bf5ec32f0cce01e57a5d884d906acc8286b903c
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-03-12 12:35:28 -0600

    set up sequelize db connection

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+exports.DB_USER = process.env.DB_USER;
+exports.DB_PASS = process.env.DB_PASS;
+exports.DB_URL = process.env.DB_URL;
+exports.DB_PORT = process.env.DB_PORT;
+exports.DB_NAME = process.env.DB_NAME;
--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const dev = app.get('env') !== 'production';
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5001);
 
+// DB Connection
+require('dotenv').config();
+const { DB_USER, 
+        DB_PASS, 
+        DB_URL, 
+        DB_PORT, 
+        DB_NAME } = require('./config');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_URL}:${DB_PORT}/${DB_NAME}`);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Production and Dev config
 if(!dev) {
     app.disable('x-powered-by');
     app.use(compression());
